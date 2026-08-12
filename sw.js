7d478cc4ccd8dcbd1509cee06f6902d09af33b3d
vsvsp <vsvspm@gmail.com>
2026-08-12 23:29:58 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "darshana-sethu-cab-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+      })
+      .catch(() => caches.match("./index.html"))
+  );
+});
