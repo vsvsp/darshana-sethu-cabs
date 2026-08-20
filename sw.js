8c2cffa04c41415d3d9772df66af573854aaf3f0
vsvsp <vsvspm@gmail.com>
2026-08-21 01:37:44 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,60 @@
-const CACHE_NAME = "darshana-sethu-cab-v1";
+const CACHE_NAME = "darshana-sethu-v1";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./sw.js"
 ];
 
-self.addEventListener("install", event => {
+// Install
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
+      .then((cache) => cache.addAll(APP_FILES))
       .then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", event => {
+// Activate
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
+    caches.keys().then((keys) =>
       Promise.all(
         keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
       )
     ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener("fetch", event => {
+// Fetch
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request)
-      .then(cachedResponse => {
-        return cachedResponse || fetch(event.request);
-      })
-      .catch(() => caches.match("./index.html"))
+    caches.match(event.request).then((cachedResponse) => {
+      return (
+        cachedResponse ||
+        fetch(event.request)
+          .then((response) => {
+            if (
+              response &&
+              response.status === 200 &&
+              response.type === "basic"
+            ) {
+              const responseClone = response.clone();
+
+              caches.open(CACHE_NAME).then((cache) => {
+                cache.put(event.request, responseClone);
+              });
+            }
+
+            return response;
+          })
+          .catch(() => caches.match("./index.html"))
+      );
+    })
   );
 });
